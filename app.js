299e42f2b77b7d75d17b80da1466802eb67df887
ConnorS17 <84247436+ConnorS17@users.noreply.github.com>
2021-05-17 14:18:54 -0230

    Add files via upload
    Updated logic for formation display

--- app.js
+++ app.js
@@ -2,20 +2,211 @@ class Formation{
 
     constructor(formationText){
         this.formationText = formationText;
+        
+        this.uniquePositions = null;
+        this.defenders = null;
+        this.midfielders = null;
+        this.strikers = null;
+        this.insideForwards = null;
+        this.defensiveMidfielders = null;
+        this.team = '';
 
     }
+
+    updateFormation(formationButtonText){
+        this.formationText = formationButtonText;
+        this.formationTextArray = formationButtonText.split("-");
+        this.uniquePositions = this.formationTextArray.length;
+    }
+
+    displayFormation(formationButtonText){
+        this.updateFormation(formationButtonText);
+        let shirtNum = 2;
+        let numDefenders = 0;
+        let numMidfielders = 0;
+        let numDMidfielders = 0;
+        let numAMidfielders = 0;
+        let numStrikers = 0;
+
+        const homeFormation = document.createElement("div");
+        homeFormation.classList.add('home-formation');
+        formationContainer.appendChild(homeFormation);
+
+        const gks = document.createElement("div");
+        gks.classList.add('Goalkeepers');
+        homeFormation.appendChild(gks);
+        const gk = document.createElement("div");
+        gk.classList.add('Goalkeeper');
+        gks.appendChild(gk);
+        gk.innerText = 1;
+        gk.setAttribute("contenteditable", "true");
+
+        const defenders = document.createElement("div");
+        const midfielders = document.createElement("div");
+        const strikers = document.createElement("div");
+
+        switch (this.uniquePositions) {
+            case 3:
+                numDefenders = parseInt(this.formationTextArray[0]);
+                numMidfielders = parseInt(this.formationTextArray[1]);
+                numStrikers = parseInt(this.formationTextArray[2]);
+                
+                
+                defenders.classList.add('Defenders');
+                homeFormation.appendChild(defenders);
+
+                for (let index = 0; index < numDefenders; index++) {
+                    const defender = document.createElement("div");
+                    defender.classList.add('Defender');
+                    defenders.appendChild(defender);
+                    defender.innerText = shirtNum;
+                    shirtNum += 1;  
+                    defender.setAttribute("contenteditable", "true");
+                }  
+                
+                
+                midfielders.classList.add('Midfielders');
+                homeFormation.appendChild(midfielders);
+            
+                for (let index = 0; index < numMidfielders; index++) {
+                    const midfielder = document.createElement("div");
+                    midfielder.classList.add('Midfielder');
+                    midfielders.appendChild(midfielder);
+                    midfielder.innerText = shirtNum;  
+                    shirtNum += 1;
+                    midfielder.setAttribute("contenteditable", "true");
+                }
+            
+                
+                strikers.classList.add('Strikers');
+                homeFormation.appendChild(strikers);
+            
+                for (let index = 0; index < numStrikers; index++) {
+                    const striker = document.createElement("div");
+                    striker.classList.add('Striker');
+                    strikers.appendChild(striker);
+                    striker.innerText = shirtNum; 
+                    shirtNum += 1;
+                    striker.setAttribute("contenteditable", "true");
+                }
+
+                break;
+
+            case 4:
+                numDefenders = parseInt(this.formationTextArray[0]);
+                numDMidfielders = parseInt(this.formationTextArray[1]);
+                numAMidfielders = parseInt(this.formationTextArray[2]);
+                numStrikers = parseInt(this.formationTextArray[3]);
+                
+                
+                defenders.classList.add('Defenders');
+                homeFormation.appendChild(defenders);
+
+                for (let index = 0; index < numDefenders; index++) {
+                const defender = document.createElement("div");
+                defender.classList.add('Defender');
+                defenders.appendChild(defender);
+                defender.innerText = shirtNum;
+                shirtNum += 1;  
+                defender.setAttribute("contenteditable", "true");
+                }  
+                
+               
+                midfielders.classList.add('Midfielders');
+                homeFormation.appendChild(midfielders);
+            
+                for (let index = 0; index < numDMidfielders; index++) {
+                const midfielder = document.createElement("div");
+                midfielder.classList.add('Midfielder');
+                midfielders.appendChild(midfielder);
+                midfielder.innerText = shirtNum;  
+                shirtNum += 1;
+                midfielder.setAttribute("contenteditable", "true");
+                }
+
+                const aMidfielders = document.createElement("div");
+                aMidfielders.classList.add('Midfielders');
+                homeFormation.appendChild(aMidfielders);
+            
+                for (let index = 0; index < numAMidfielders; index++) {
+                const aMidfielder = document.createElement("div");
+                aMidfielder.classList.add('Midfielder');
+                aMidfielders.appendChild(aMidfielder);
+                aMidfielder.innerText = shirtNum;  
+                shirtNum += 1;
+                aMidfielder.setAttribute("contenteditable", "true");
+                }
+            
+                
+                strikers.classList.add('Strikers');
+                homeFormation.appendChild(strikers);
+            
+                for (let index = 0; index < numStrikers; index++) {
+                    const striker = document.createElement("div");
+                    striker.classList.add('Striker');
+                    strikers.appendChild(striker);
+                    striker.innerText = shirtNum; 
+                    shirtNum += 1;
+                    striker.setAttribute("contenteditable", "true");
+                }
+
+                break;
+
+                default:
+                    console.log("uh oh");
+
+        }
+
+    }
+
+
 }
 
 const formationContainer = document.querySelector(".formation");
 const clearButton = document.querySelector(".clear");
-const fourFourTwoButton = document.querySelector(".four-four-two");
+const formationButtons = document.querySelectorAll(".formation-button");
 
-clearButton.addEventListener("click", displayClear );
+let formationText ;
+const formation = new Formation(formationText);
 
-fourFourTwoButton.addEventListener("click", displayFourFourTwo);
+clearButton.addEventListener("click", displayClear);
 
 
-function displayFourFourTwo(){
+
+formationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        formation.displayFormation(button.innerText)
+    })
+})
+
+function displayClear(){
+    /*let delG = document.querySelector('.Goalkeepers');
+    let delD = document.querySelector('.Defenders');
+    let delM = document.querySelector('.Midfielders');
+    let delA = document.querySelector('.Strikers');
+
+    delG.remove();
+    delD.remove();
+    delM.remove();
+    delA.remove();*/
+
+    let delFormation = document.querySelector('.home-formation');
+    delFormation.remove();
+}
+
+
+//fourFourTwoButton.addEventListener("click", displayFourFourTwo);
+//fourFourTwoButton.addEventListener("click", updateFormation(fourFourTwoButton));
+
+//function updateFormation(button){
+   // textArray = button.innerText.split("-");
+    //formationTextArray = textArray;
+
+//}
+
+
+
+/*function displayFourFourTwo(){
     
     const gks = document.createElement("div");
     gks.classList.add('Goalkeepers');
@@ -69,17 +260,5 @@ function displayFourFourTwo(){
         striker.innerText = index; 
         striker.setAttribute("contenteditable", "true");
     }
-}
+}*/
 
-function displayClear(){
-    let delG = document.querySelector('.Goalkeepers');
-    let delD = document.querySelector('.Defenders');
-    let delM = document.querySelector('.Midfielders');
-    let delA = document.querySelector('.Strikers');
-
-    delG.remove();
-    delD.remove();
-    delM.remove();
-    delA.remove();
-
-}
